7272ce1daae2a463e53c166f28216b37f1b6ccf2
zhouzhuojie <zhouzhuojie@gmail.com>
2020-05-23 00:10:58 -0700

    Add webhook callback template

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/dsrhub/dsrhub
-
-go 1.14
-
-require github.com/ovh/utask v1.6.0
--- vendor/github.com/ovh/utask/db/sqlgenerator/sqlgenerator.go
+++
@@ -1,6 +0,0 @@
-package sqlgenerator
-
-import "github.com/Masterminds/squirrel"
-
-// PGsql is a shortcut to a dollar-based statement builder
-var PGsql = squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
