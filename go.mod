f082d894791270b7568301ed7b7a8aca7ea92c0e
zhuojie <zhuojie@checkr.com>
2020-06-24 17:52:58 -0700

    Add grpc plugin

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.2
+	github.com/golang/protobuf v1.4.0
 	github.com/loopfz/gadgeto v0.10.0
 	github.com/ovh/utask v1.6.0
 	github.com/sirupsen/logrus v1.5.0
+	github.com/stretchr/testify v1.5.1
 	github.com/wI2L/fizz v0.13.4
+	golang.org/x/net v0.0.0-20190921015927-1a5e07d1ff72
+	google.golang.org/grpc v1.21.0
 )
---
+++ plugins/dsrhub_grpc/dsrhub_grpc.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/dsrhub/dsrhub/idl_dsrhub"
+	"github.com/ovh/utask/pkg/plugins/taskplugin"
+	"google.golang.org/grpc"
+)
+
+var (
+	// Plugin is the dsrhub_grpc plugin that we can use as a customized plugin
+	// nolint
+	Plugin = taskplugin.New(
+		"dsrhub_grpc", "1.0", exec,
+		taskplugin.WithConfig(validConfig, DsrHubGRPCConfig{}),
+	)
+)
+
+// DsrHubGRPCConfig is the configuration needed to perform an gRPC client side call
+type DsrHubGRPCConfig struct {
+	URL     string                      `json:"url"`
+	Request idl_dsrhub.CreateDSRRequest `json:"request"`
+	Timeout int                         `json:"timeout,omitempty"` // timeout in seconds
+}
+
+func validConfig(config interface{}) error {
+	cfg := config.(*DsrHubGRPCConfig)
+	if cfg.URL == "" {
+		return fmt.Errorf("invalid dsrhub_grpc config url: empty url")
+	}
+	if cfg.Request.ApiVersion == "" {
+		return fmt.Errorf("invalid dsrhub_grpc config request.api_version: empty request.api_version")
+	}
+	if cfg.Request.Regulation != "gdpr" && cfg.Request.Regulation != "ccpa" {
+		return fmt.Errorf("invalid dsrhub_grpc config request.regulation: %s, want: [gdpr, ccpa]", cfg.Request.Regulation)
+	}
+	if cfg.Request.StatusCallbackUrl == "" {
+		return fmt.Errorf("invalid dsrhub_grpc config request.status_callback_url: empty request.status_callback_url")
+	}
+	if cfg.Request.SubjectRequestId == "" {
+		return fmt.Errorf("invalid dsrhub_grpc config request.subject_request_id: empty request.subject_request_id")
+	}
+	if cfg.Request.SubjectRequestType == "" {
+		return fmt.Errorf("invalid dsrhub_grpc config request.subject_request_type: empty request.subject_request_type")
+	}
+	if cfg.Request.IdentityValue == "" {
+		return fmt.Errorf("invalid dsrhub_grpc config request.identity_value: empty request.identity_value")
+	}
+	return nil
+}
+
+func exec(stepName string, config interface{}, execCtx interface{}) (output interface{}, metadata interface{}, err error) {
+	cfg := config.(*DsrHubGRPCConfig)
+
+	// TODO: support secure connection with configuration
+	conn, err := grpc.Dial(cfg.URL, grpc.WithInsecure())
+	if err != nil {
+		return nil, nil, err
+	}
+	defer conn.Close()
+
+	ctx, cancel := context.WithDeadline(
+		context.TODO(),
+		time.Now().Add(time.Duration(cfg.Timeout)*time.Second),
+	)
+	defer cancel()
+
+	res, err := idl_dsrhub.NewOpenDSRServiceClient(conn).CreateDSR(ctx, &cfg.Request)
+	if err != nil {
+		return nil, nil, err
+	}
+	return res, nil, nil
+}
